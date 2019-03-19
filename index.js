4e90620132a58c94b33b9deed27eaf32a4a0ba7b
Andreas Kaae <anka.skp@edu.mercantec.dk>
2019-03-19 14:41:23 +0100

    adding pages

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const path = require('path')
 
 const app = express()
 
-    app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) =>
+res.render('index', {
+    title: 'Home'
+    // add module to
+}))
 
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000
 
