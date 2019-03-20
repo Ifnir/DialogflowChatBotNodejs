60c648f570210ef950d60bf73c3c5a034d87e4ce
Andreas Kaae <anka.skp@edu.mercantec.dk>
2019-03-20 08:39:29 +0100

    handlebars installed Now firebase auth before next

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const path = require('path')
-
+const exphbs = require('express-handlebars')
+const auth = require('./middleware/auth')
 const app = express()
+// run middleware for firebase login
 
-app.get('/', (req, res) =>
-res.render('index', {
-    title: 'Home'
-    // add module to
-}))
+// View Engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set("view engine", "handlebars")
+
+app.get('/', (req, res) => {
+
+    res.render("index", {
+        title: 'Home'
+    })
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 
---
+++ middleware/auth.js
@@ -0,0 +1,5 @@
+const auth = (req, res, next) => {
+    let i = 1
+}
+
+module.exports = auth
