3fd22066a80c0db186366da4db0e6fc488fa19d7
Andreas Kaae <anka.skp@edu.mercantec.dk>
2019-03-20 10:48:40 +0100

    clean it up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
-const auth = require('./middleware/auth')
+const firebaseApp = require('./middleware/auth')
 const app = express()
 // run middleware for firebase login
 
@@ -9,17 +9,30 @@ const app = express()
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set("view engine", "handlebars")
 
+
+const middleware = (req, res, next) => {
+    var user = firebaseApp.auth().currentUser
+    if (req.user == null) {
+        req.user = user
+        next()
+    } else {
+        res.redirect('/')
+    }
+}
+
 app.get('/', (req, res) => {
     res.render("index", {
         title: 'Home'
     })
 })
 
-app.get('/welcome', (req, res) => {
+app.get('/welcome', middleware, (req, res) => {
     res.render("welcome", {
-        title: 'Welcome'
+    title: 'Welcome',
     })
 })
+    
+
 
 app.get('/chat', (req, res) => {
     res.render("chat", {
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,19 @@
-const auth = (req, res, next) => {
-    let i = 1
-}
+var firebase = require('firebase')
+require('firebase/auth');
+require('firebase/database');
 
-module.exports = auth
+var config = {
+    apiKey: "AIzaSyBY36a7dh1a0YmkkRyaH8UpBi2IynelTMY",
+    authDomain: "nodejscb.firebaseapp.com",
+    databaseURL: "https://nodejscb.firebaseio.com",
+    projectId: "nodejscb",
+    storageBucket: "nodejscb.appspot.com",
+    messagingSenderId: "85764044261"
+  };
+
+
+var firebaseApp = firebase.initializeApp(config);
+
+
+
+module.exports = firebaseApp
