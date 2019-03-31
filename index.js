d2c3bd421c39d2c9098debf19cde0c5c5a037a9e
Andreas <demikz.co@gmail.com>
2019-03-31 19:46:51 +0200

    work monday

--- index.js
+++ index.js
@@ -2,33 +2,34 @@ const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const app = express()
-const diarre = require('./dialogflow/init')
+
 // run middleware for firebase login
 
 // View Engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set("view engine", "handlebars")
 
+const server = require('http').createServer(express)
+const io = require('socket.io')(server)
 
-    
-
-
-
-
-
-
+// io.on('connection', function(socket){
+//     console.log('a user connected' + socket);
+// })
+/**
+ * https://itnext.io/building-a-node-js-websocket-chat-app-with-socket-io-and-react-473a0686d1e1
+ * 
+ * make a new serverbackend for socket
+ */
 
 app.get('/', (req, res) => {
-    diarre().then((dar) => {
-        res.render("chat", {
-            title: 'Chat',
-            prop: dar
-        })
+    res.render("chat", {
+        title: 'Chat',
     })
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 3000
+
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
